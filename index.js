c7700a333491d5d6e53970d1a5ef43327215936c
Jeff Mahoney <jeff.t.mahoney@gmail.com>
2022-08-02 22:19:22 -0400

    Read and write file examples
    * Examples of creating and reading from files that hold portions (or all) of the data returned from the MNIST sets.

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
+const fs = require('fs')
 const mnist = require('mnist')
 
-const set = mnist.set(40, 10)
-
+const set = mnist.set(4000, 1000)
 const trainingSet = set.training
 const testSet = set.test
 
-console.log('# of entries: ', trainingSet.length)
-//console.log('trainingSet', trainingSet)
+// one way to do filtering
+const theSevens = trainingSet.filter(
+  (x) => x.output.join(',') === '0,0,0,0,0,0,0,1,0,0'
+)
 
-// get all the 7s
-const theSevens = trainingSet.filter(x => x.output.join(',') === '0,0,0,0,0,0,0,1,0,0')
+// create a training file source
+const trainingContent = trainingSet.map((x) => JSON.stringify(x)).join('\n')
+fs.writeFileSync('data/training_source.json', trainingContent)
 
-console.log('How many sevens?')
-console.log(theSevens.length)
+// create a test file source
+const testContent = testSet.map((x) => JSON.stringify(x)).join('\n')
+fs.writeFileSync('data/test_source.json', testContent)
 
+// read content back and use it
+const receivedContent = () =>
+  fs.readFileSync('data/data.json', { encoding: 'ascii' })
+const jsonify = (content) => content.split('\n').map((x) => JSON.parse(x))
 
+console.log('receivedContent', jsonify(receivedContent()))
